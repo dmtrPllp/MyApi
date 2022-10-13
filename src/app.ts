3e77a84d55c2733d6e1706fabb90f9b3ee30b7b8
Dmitry <dmitry.p00.02@mail.ru>
2022-10-13 11:33:08 +0300

    dto

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'inversify';
 import { ExceptionFilter } from './errors/exception-filter';
 import { ILogger } from './logger/logger-interface';
 import { TYPES } from './types';
+import { json } from 'body-parser';
 import { UserController } from './users/users-controller';
 import 'reflect-metadata';
 
@@ -22,6 +23,10 @@ export class App {
 		this.port = 5000;
 	}
 
+	useMiddleware(): void {
+		this.app.use(json());
+	}
+
 	useRoutes(): void {
 		this.app.use('/users', this.userController.router);
 	}
@@ -31,6 +36,7 @@ export class App {
 	}
 
 	public async init(): Promise<void> {
+		this.useMiddleware();
 		this.useRoutes();
 		this.useExceptionFilters();
 		this.server = this.app.listen(this.port);
--- src/users/users-controller.ts
+++ src/users/users-controller.ts
@@ -6,6 +6,9 @@ import { ILogger } from '../logger/logger-interface';
 import { TYPES } from '../types';
 import 'reflect-metadata';
 import { IUser } from './user-interface';
+import { UserLoginDto } from './dto/user-login_dto';
+import { UserRegisterDto } from './dto/user-register_dto';
+import { User } from './user-entity';
 
 @injectable()
 export class UserController extends BaseController implements IUser {
@@ -17,11 +20,18 @@ export class UserController extends BaseController implements IUser {
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 		//this.ok<string>(res,'login is OK!');
 	}
-	register(req: Request, res: Response, next: NextFunction): void {
-		this.ok<string>(res, 'reg is OK!');
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok(res, newUser);
 	}
 }
