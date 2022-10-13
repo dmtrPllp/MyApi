139d43a1d0bd9c1b4af6a0a8e795f5bcbce6bd68
Dmitry <dmitry.p00.02@mail.ru>
2022-10-13 12:04:58 +0300

    middleware interface

--- src/common/IRoute.ts
+++ src/common/IRoute.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { IMiddleWare } from './IMiddleware';
 
 export interface IControllerRoute {
 	path: string;
 	func: (req: Request, res: Response, next: NextFunction) => void;
 	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'put' | 'patch'>;
+	middlewares?: IMiddleWare[];
 }
 
 export type ExpressReturnType = Response<any, Record<string, any>>;
